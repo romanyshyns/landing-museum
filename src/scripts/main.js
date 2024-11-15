2c34d6a3ee58e9ace7141e2c569b4141912c2d77
Stanislav Romanyshyn <romanyshyn6696@gmail.com>
2024-11-15 12:29:22 +0200

    clone landing museum

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+document.querySelectorAll('.navigation .bar').forEach((label, index) => {
+  label.addEventListener('click', () => {
+    document.querySelectorAll('input[name="r"]')[index].checked = true;
+  });
+});
