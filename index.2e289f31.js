3e0243435c53c10374d62222d3f694971dea31ac
Stanislav Romanyshyn <romanyshyn6696@gmail.com>
2024-11-15 12:30:44 +0200

    make build

---
+++ index.2e289f31.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".navigation .bar").forEach(function(e,n){e.addEventListener("click",function(){document.querySelectorAll('input[name="r"]')[n].checked=!0})});
+//# sourceMappingURL=index.2e289f31.js.map
